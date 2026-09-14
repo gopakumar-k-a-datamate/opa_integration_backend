cc8c03085fab6409c431406ba1b7fd28e762390e
Gopakumar. K.A <gopakumar.k@mediwarehms.com>
2026-09-14 14:32:10 +0530

    allow all origins during development for using (Admin UI and Flutter app)

--- identity-service/src/main/java/org/datamate/identity/shared/config/SecurityConfig.java
+++ identity-service/src/main/java/org/datamate/identity/shared/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Admin UI
+        configuration.setAllowedOriginPatterns(List.of("*")); // Admin UI and Flutter app
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
